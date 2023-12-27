be1e6872401cc56a03c44be3a894fc48fee0909e
Camila <camimq85@gmail.com>
2023-12-27 15:28:09 -0300

    exclui arquivos que não serão utilizados

--- analises/producao.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Importando os dados da base de Produção
-df_producao = pd.read_csv('https://raw.githubusercontent.com/camimq/techChallenge_exporta_vinhos/main/bases/Producao.csv', sep=';')
