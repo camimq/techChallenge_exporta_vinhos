d354a9a37d5e996390a769486173d48d6c192016
Camila <camimq85@gmail.com>
2024-01-03 16:34:43 -0300

    atualiza configurações da página set_page_config

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Dados brutos | TechChallenge FIAP :wine_glass:', page_icon='https://cdn-icons-png.flaticon.com/512/763/763048.png')
+st.set_page_config(page_title='Dados brutos | TechChallenge FIAP :wine_glass:', page_icon='https://cdn-icons-png.flaticon.com/512/763/763048.png', layout='wide')
 
 # tabela de dado que será utilizada na exibição da aplicação
 st.title('Dados brutos')
