ebb416ddbc930b8ebc77ccdb27fb66ab8714492f
Camila Queiroz <camimq85@gmail.com>
2024-01-06 19:12:37 -0300

    atualiza informações sobe página de dados brutos.

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Dados brutos | TechChallenge FIAP :wine_glass:', page_icon='https://cdn-icons-png.flaticon.com/512/763/763048.png', layout='wide')
+st.set_page_config(page_title='Dados brutos | TechChallenge FIAP', page_icon='https://cdn-icons-png.flaticon.com/512/763/763048.png', layout='wide')
+
+with st.sidebar:     
+    st.markdown(':sweat_smile: Feito por **Camila Queiroz - RM 351738**')
 
 # tabela de dado que será utilizada na exibição da aplicação
 st.title('Dados brutos')
