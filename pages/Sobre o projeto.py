368002d5ca1382e8b62ffea18b846c927e318139
Camila Queiroz <camimq85@gmail.com>
2024-01-06 21:12:42 -0300

    finaliza página sobre o projeto

--- pages/Sobre o projeto.py
+++ pages/Sobre o projeto.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title='Sobre o Projeto | TechChallenge FIAP', page_icon='https://cdn-icons-png.flaticon.com/512/763/763048.png')
 
-st.image('https://www.freewebheaders.com/wp-content/gallery/drinks/awesome-wine-barrel-and-bottle-with-red-wine-glasses-web-header.jpg', caption='Foto retirada de Free Webheaders.com')
+st.image('img\header.jpg', caption='Imagem criada pelo Copilot do Edge')
 
 with st.sidebar:     
     st.markdown(':sweat_smile: Feito por **Camila Queiroz - RM 351738**')
@@ -26,10 +26,36 @@ with col21:
             
             O montante de exportação dos ***últimos 15 anos (2008 à 2022)** será avaliado, através das técnicas e conceitos apresentados durante o primeiro módulo do curso, de forma que, ao fim desta análise, tenhamos um panorama geral e um plano de ação para que o Brasil siga se consolidando como um dos principais atores no mercado de exportação de vinhos e derivados.
         ''')
+
+st.markdown('---')
+st.markdown('## Referências bibliográficas')
+st.markdown('Abaixo, listo as fontes pesquisadas para a realização deste projeto:')
+st.markdown('''
+                - Vinho e Colonização conta a história da bebida durante  colonização portuguesa no Brasil
+                    https://brasildevinhos.com.br/vinho-e-colonizacao-conta-a-historia-da-bebida-durante-colonizacao-portuguesa-no-brasil/
+
+                - O Vinho no Brasil                                                                             
+                    http://vinhobrasileiro.org/o-vinho-no-brasil/historia
+
+                - G1: E o maior produtor mundial de vinho é...
+                    https://g1.globo.com/pr/parana/especial-publicitario/porto-a-porto/guia-do-vinho-e-da-gastronomia/noticia/2023/10/03/e-o-maior-produtor-mundial-de-vinho-e.ghtml
+
+                - Gazeta do Povo: Exportação de vinhos brasileiros avança 325% em cinco anos para os EUA e China
+                    https://www.gazetadopovo.com.br/bomgourmet/negocios-e-franquias/exportacao-vinhos-brasileiros-avanca-eua-china/
+
+                - Associação Brasileira de Enologia: Uma porta aberta para os vinhos brasileiros no Reino Unido
+                    https://www.enologia.org.br/noticia/uma-porta-aberta-para-os-vinhos-brasileiros-no-reino-unido
+
+                - G1: De olho na Copa, importadores britânicos miram no vinho brasileiro
+                    https://g1.globo.com/brasil/noticia/2013/08/de-olho-na-copa-importadores-britanicos-miram-no-vinho-brasileiro.html
+
+                - RBA: Crise no Paraguai é agravada pela situação econômica do país
+                    https://www.redebrasilatual.com.br/mundo/crise-paraguai-situacao-economica/
+            ''')
  
 st.markdown('')
 st.write('---')
-st.write('## Quem fez :technologist:')
+st.write('### Quem fez :technologist:')
 col3, col4 = st.columns(2)
 with col3:
     st.write('**Autor:** Camila Queiroz - RM 351738')
@@ -39,3 +65,4 @@ with col3:
      [![Title](data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABDUlEQVR4AWP4////gOLB44D6nTcsGIo33QHi/zTGd0B2YTiAPpYjHIHNAf/piQk6wGPW8f/rLz8HYRCbXg5AWI4GQGJ0cwDY12gAJDbcHUA4CkZAIqQUK7Ts/m/SfxBMs5RupswBaACr+P47b/5zlG/5DyzZ/r/+8hNF7vuvP//nn3r0X6JhJ+0ccPrR+/+H7735jw9cf/n5v0D1Nuo5gBxQve06zR0AjoL7b7/+//zjN4bc+ScfaOeA33///k9Yfg4mDw7u/Xdeo6uhnQP6D93FMNxlxjF0ZbRzgMXEQ9iyI90cALIMJoccDXRzAK6CZog6YNQBow6gIx54Bwx4x2RAu2bAysoEZu9o7xgAQrvkxt3WZi0AAAAASUVORK5CYII=)](www.linkedin.com/in/camilaqueiroz) [![Repo](data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAb1BMVEX////4+Pi3ubtvcnZNUVU+Q0cpLjLr6+x3en0sMTYkKS59gIORk5aUl5n8/Pzw8PFTV1tbX2Pc3d5DSEzn5+g3PECLjpFKTlKFh4qxs7XCxMUwNTq/wcLh4uPV1tZzd3o/Q0jOz9CmqKpjZ2qfoaSrd37mAAABPUlEQVR4AW3TBZKEMBAF0B8GCHzcnbW5/xm30qEyknklcU/DgQpuYRTHUXgLFHw6SemkmcYrlcd8kRYlnlQ1PU0Fp434Qde75Qd+1FUQKiRZjyGfTGNjKhWMmSQXYO3Ibao3MlqBnSRzADhk/ycAdcqclSSHnEUD+KLt8KalMQMqpl3izU5jKxHQGCq8Ud80fq4VfuFZaIyQO4wVPEre5g+RrIAPJrkQSL8OPjv3htQmH8guU5uwgseeP7ITMYBnpdFgvlJPcx0zoLjjzS/FDrVRvH6xsqDYlLx29huRUaFx6YuI1mhKMbddf9trEzca7rmRk/FxpiRXiJO8FDBURyb4yfO7glC8TOpacmAc4ElMEWlc2oGckjwvYVFEB5wjouE6uLBwquypQym/scKrM4njElYaJy182q15aDj/oQMZkS8JH3IAAAAASUVORK5CYII=)](https://github.com/camimq/camimq)
     '''
     st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown('[:arrow_up_small:[Topo da página]](#e0c02a73)')
